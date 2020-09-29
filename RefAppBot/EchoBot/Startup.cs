1a21f908ec82aa7e6a52ee1a45ff56da06cefb62
Shravan Kumar <b.j.shravan.kumar@philips.com>
2020-09-29 14:15:50 +0530

    Puching ref app echo bot

---
+++ RefAppBot/EchoBot/Startup.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.BotBuilderSamples.Bots;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, EchoBot>();
+
+            services.AddSingleton(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = "07dfcc61-3f61-4266-9ae2-0b02fe54dcf0",
+                EndpointKey = "2d6250df-0986-48be-8f9b-fde8507b3c5b",
+                Host = "https://qna-euw1-xlncare-uk-01-np.azurewebsites.net/qnamaker"
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            // app.UseHttpsRedirection();
+        }
+    }
+}
