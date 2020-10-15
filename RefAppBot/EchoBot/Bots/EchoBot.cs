2ba9dea8ef1a6d2291075bc56814ef880e22d638
Shravana <b.j.shravan.kumar@philips.com>
2020-10-15 15:04:35 +0530

    Echo bot with DB storage

--- RefAppBot/EchoBot/Bots/EchoBot.cs
+++ RefAppBot/EchoBot/Bots/EchoBot.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) Philips. All rights reserved.
+﻿// Copyright (c) Philips Corporation. All rights reserved.
+//
 
 using System.Collections.Generic;
 using System.Threading;
@@ -7,38 +8,105 @@ using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 using Microsoft.Bot.Builder.AI.QnA;
 using System.Linq;
+using System;
+using Newtonsoft.Json;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
     public class EchoBot : ActivityHandler
     {
+        private BotState _conversationState;
+        private BotState _userState;
+
         public QnAMaker EchoBotQnA { get; private set; }
-        public EchoBot(QnAMakerEndpoint endpoint)
+
+        public EchoBot(QnAMakerEndpoint endpoint,ConversationState conversationState, UserState userState)
         {
             // connects to QnA Maker endpoint for each turn
             EchoBotQnA = new QnAMaker(endpoint);
+            _conversationState = conversationState;
+            _userState = userState;
         }
+
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             var results = await EchoBotQnA.GetAnswersAsync(turnContext);
+
+            var conversationStateAccessors = _conversationState.CreateProperty<ConversationData>(nameof(ConversationData));
+            var conversationData = await conversationStateAccessors.GetAsync(turnContext, () => new ConversationData());
+
+            var userStateAccessors = _userState.CreateProperty<UserProfile>(nameof(UserProfile));
+            var userProfile = await userStateAccessors.GetAsync(turnContext, () => new UserProfile());
+            var messageTimeOffset = (DateTimeOffset)turnContext.Activity.Timestamp;
+            var localMessageTime = messageTimeOffset.ToLocalTime();
+            conversationData.Timestamp = localMessageTime.ToString();
+            conversationData.ChannelId = turnContext.Activity.ChannelId.ToString();
+            conversationData.InConversationString = turnContext.Activity.Text;
+            userProfile.conversations.Add(conversationData);
+            userProfile.conversationID = turnContext.Activity.Id;
+
             if (results.Any())
             {
                 var reply = MessageFactory.Text(results.First().Answer);
-                if (results.First().Context.Prompts.Length > 0)
+                var aQnAResult = results.First();
+                var resultMetadata = aQnAResult.Metadata;
+                if (aQnAResult.Context.Prompts.Length > 0)
                 {
                     var suggestedActions = new SuggestedActions();
                     reply.SuggestedActions = suggestedActions;
                     reply.SuggestedActions.Actions = new List<CardAction>();
                     for (int i = 0; i < results.First().Context.Prompts.Length; i++)
                     {
-                        var promptText = results.First().Context.Prompts[i].DisplayText;
-                        reply.SuggestedActions.Actions.Add(item: new CardAction() { Title = promptText, Type = ActionTypes.ImBack, Value = promptText });
+                        string promptImageURL = null;
+                        foreach (var promptMetaData in resultMetadata)
+                        {
+                            var optionNumber = i + 1;
+                            if (promptMetaData.Name == optionNumber.ToString())
+                            {
+                                promptImageURL = "https://" + promptMetaData.Value;
+                            }
+                        }
+                        //https://i.ibb.co/XSCmhvT/5b58db84b354cd1f008b45ce.jpg
+                        var promptText = aQnAResult.Context.Prompts[i].DisplayText;
+                        reply.SuggestedActions.Actions.Add(item: new CardAction() { Title = promptText, Type = ActionTypes.ImBack, Value = promptText, Image = promptImageURL });
                     }
                 }
-
+                conversationData.OutConversationString = reply.Text;
                 // Respond with the result from the Qna Service
                 await turnContext.SendActivityAsync(reply, cancellationToken);
             }
+            else
+            {
+                // The QnA service did not have any results
+                // Proceed with the previous code
+                var aText = "Sorry was not able to understand the question. Please select below devices to continue.";
+                var aCard = MessageFactory.Text(aText);
+                var suggestedActions = new SuggestedActions();
+                aCard.SuggestedActions = suggestedActions;
+                aCard.SuggestedActions.Actions = new List<CardAction>();
+                var suggestion1 = "Tuscany";
+                var suggestion2 = "Osla";
+                aCard.SuggestedActions.Actions.Add(item: new CardAction() { Title = suggestion1, Type = ActionTypes.ImBack, Value = suggestion1 });
+                aCard.SuggestedActions.Actions.Add(item: new CardAction() { Title = suggestion2, Type = ActionTypes.ImBack, Value = suggestion2 });
+                await turnContext.SendActivityAsync(aCard, cancellationToken);
+            }
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            var activity = turnContext.Activity;
+
+            if (string.IsNullOrWhiteSpace(activity.Text) && activity.Value != null)
+            {
+                activity.Text = JsonConvert.SerializeObject(activity.Value);
+            }
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            // Save any state changes that might have occured during the turn.  
+            await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await _userState.SaveChangesAsync(turnContext, false, cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
@@ -47,24 +115,52 @@ namespace Microsoft.BotBuilderSamples.Bots
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    var welcomeText = "Hi! I am the SleepMapper assistant.I can help you with the connectivity issues you are facing with your device"; //"Hi, I am your Philips assistant and welcome!. Please select the product to assist you." + "\n\n" + "Philips Privacy Policy : " + "https://www.philips.com/a-w/privacy-notice.html";
+                    //Welcome Card
+                    var welcomeText = "Hi! I am your Philips Sonicare Brush advice bot."; //"Hi, I am your Philips assistant and welcome!. Please select the product to assist you." + "\n\n" + "Philips Privacy Policy : " + "https://www.philips.com/a-w/privacy-notice.html";
                     var welcomeCard = MessageFactory.Text(welcomeText);
                     await turnContext.SendActivityAsync(welcomeCard, cancellationToken);
+
+                    //Welcome card two
+                    var welcomeText1 = "I can help you to find out the best product and I can also resolve the connectivity issues you are facing with your device"; //"Hi, I am your Philips assistant and welcome!. Please select the product to assist you." + "\n\n" + "Philips Privacy Policy : " + "https://www.philips.com/a-w/privacy-notice.html";
+                    var welcomeCard1 = MessageFactory.Text(welcomeText1);
+                    _ = await turnContext.SendActivitiesAsync(
+                             new Activity[] {
+                                    new Activity { Type = "delay", Value= 1000 },
+                                    welcomeCard1,
+                            },
+                    cancellationToken);
+                   
+                   
+
                     //Privacy card..
-                    var privacyText = "**Philips Privacy Notice** A transcript of this chat is retained with us for quality and training purposes.Chat data collected may be linked to case reference id in cases where the assistant is unable to resolve the query and support is required from Philips Consumer Care.Philips values and respects your privacy.Please read our [privacy notice](https://www.philips.co.in/content/corporate/en_IN/privacy-notice.html/) for more information.";
+                    var privacyText = "**Philips Privacy Notice.** A transcript of this chat is retained with us for quality and training purposes.Chat data collected may be linked to case reference id in cases where the assistant is unable to resolve the query and support is required from Philips Consumer Care.Philips values and respects your privacy.Please read our [privacy notice](https://www.philips.co.in/content/corporate/en_IN/privacy-notice.html/) for more information.";
                     var privacyCard = MessageFactory.Text(privacyText);
+                    _ = await turnContext.SendActivitiesAsync(
+                             new Activity[] {
+                                                 new Activity { Type = "delay", Value= 1500 },
+                                                 privacyCard,
+                            },
+                    cancellationToken);
+
+                    //Welcome card with user options
+                    var welcomeText2 = "Please let us know what type of product you own";
+                    var welcomeCard2 = MessageFactory.Text(welcomeText2);
                     var suggestedActions = new SuggestedActions();
-                    privacyCard.SuggestedActions = suggestedActions;
-                    privacyCard.SuggestedActions.Actions = new List<CardAction>();
-                    var suggestion1 = "Tuscany";
-                    var suggestion2 = "Osla";
-                    privacyCard.SuggestedActions.Actions.Add(item: new CardAction() { Title = suggestion1, Type = ActionTypes.ImBack, Value = suggestion1 });
-                    privacyCard.SuggestedActions.Actions.Add(item: new CardAction() { Title = suggestion2, Type = ActionTypes.ImBack, Value = suggestion2 });
+                    welcomeCard2.SuggestedActions = suggestedActions;
+                    welcomeCard2.SuggestedActions.Actions = new List<CardAction>();
+                    var suggestion1 = "SoniCare SmartClean";
+                    var suggestion1CTN = "SoniCare SmartClean HX9903/11";
+                    var suggertion1Image = "https://www.usa.philips.com/c-dam/b2c/master/experience/consistency-campaign/diamondclean-smart/philips-sonicare-diamondclean_smart-with-glass_charger-black-HX9903_11.png";
+                    var suggestion2 = "SoniCare Clean";
+                    var suggestion2CTN = "SoniCare Clean HX6853/11";
+                    var suggestion2Image = "https://images.philips.com/is/image/PhilipsConsumer/HX6853_11-IMS-en_US?wid=840&hei=720&$jpglarge$";
+                    welcomeCard2.SuggestedActions.Actions.Add(item: new CardAction() { Title = suggestion1, Type = ActionTypes.ImBack, Value = suggestion1CTN, Image = suggertion1Image });
+                    welcomeCard2.SuggestedActions.Actions.Add(item: new CardAction() { Title = suggestion2, Type = ActionTypes.ImBack, Value = suggestion2CTN, Image = suggestion2Image });
 
                     _ = await turnContext.SendActivitiesAsync(
                              new Activity[] {
-                             new Activity { Type = "delay", Value= 2000 },
-                             privacyCard,
+                             new Activity { Type = "delay", Value= 3500 },
+                             welcomeCard2,
                             },
                     cancellationToken);
                     return;
@@ -72,4 +168,32 @@ namespace Microsoft.BotBuilderSamples.Bots
             }
         }
     }
+
+
+
+    public class UserProfile
+    {
+        public string conversationID { get; set; }
+        public List<ConversationData> conversations { get; } = new List<ConversationData>();
+    }
+
+    public class ConversationData
+    {
+        // The time-stamp of the most recent incoming message.  
+        public string Timestamp { get; set; }
+
+        // The ID of the user's channel.  
+        public string ChannelId { get; set; }
+
+        //User selected option ConversationString
+        public string InConversationString { get; set; }
+
+        //Out ConversationString
+        public string OutConversationString { get; set; }
+
+    }
+
 }
+
+
+
--- RefAppBot/EchoBot/Startup.cs
+++ RefAppBot/EchoBot/Startup.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,10 +12,23 @@ using Microsoft.BotBuilderSamples.Bots;
 using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Azure.Cosmos;
+using System.Threading.Tasks;
+
 namespace Microsoft.BotBuilderSamples
 {
     public class Startup
     {
+
+        //Cosmos Db Settings
+        public static string TranscriptDatabaseId { get; set; }
+        public static string TranscriptCollectionId { get; set; }
+        public static string TranscriptAuthKey { get; set; }
+        public static string CosmosDBEndpoint { get; set; }
+        public static string ConversationStateCollectionName { get; set; }
+        public static string UserStateCollectionName { get; set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,16 +36,26 @@ namespace Microsoft.BotBuilderSamples
 
         public IConfiguration Configuration { get; }
 
+        private const string CosmosServiceEndpoint = "https://pz-ew-cosmos-np-digitaltrans-001.documents.azure.com:443/";
+        private const string CosmosDBKey = "L2NPREph39uqCmawhpxJfRkTch1uigTihDH8VsNJ6ZQzq1p1oZztGfMB07ND4V31kz47d6eLIooaaXaKAgTHug==";
+        private const string CosmosDBDatabaseName = "non-prod-cosmos-db";
+        private const string CosmosDBCollectionName = "ems-chat-transcripts";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            TranscriptDatabaseId = CosmosDBDatabaseName;//Configuration.GetSection("CosmosDb:DatabaseName").Value;
+            TranscriptCollectionId = CosmosDBCollectionName;// Configuration.GetSection("CosmosDb:TranscriptCollectionName").Value;
+            TranscriptAuthKey = CosmosDBKey;// Configuration["AP-Prod-CosmosDBKey"];
+            CosmosDBEndpoint = CosmosServiceEndpoint;// Configuration.GetSection("CosmosDb:Account").Value;
+            ConversationStateCollectionName = Configuration.GetSection("CosmosDb:ConversationStateCollectionName").Value;
+            UserStateCollectionName = Configuration.GetSection("CosmosDb:UserStateCollectionName").Value;
+
             services.AddControllers().AddNewtonsoftJson();
 
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
-
-            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EchoBot>();
 
             services.AddSingleton(new QnAMakerEndpoint
             {
@@ -39,6 +63,26 @@ namespace Microsoft.BotBuilderSamples
                 EndpointKey = "2d6250df-0986-48be-8f9b-fde8507b3c5b",
                 Host = "https://qna-euw1-xlncare-uk-01-np.azurewebsites.net/qnamaker"
             });
+
+            var cosmosDbStorageOptions = new CosmosDbPartitionedStorageOptions()
+            {
+                CosmosDbEndpoint = "https://pz-ew-cosmos-np-digitaltrans-001.documents.azure.com:443/",
+                AuthKey = "L2NPREph39uqCmawhpxJfRkTch1uigTihDH8VsNJ6ZQzq1p1oZztGfMB07ND4V31kz47d6eLIooaaXaKAgTHug==",
+                DatabaseId = "non-prod-cosmos-db",
+                ContainerId = "ems-chat-transcripts"
+            };
+            var storage = new CosmosDbPartitionedStorage(cosmosDbStorageOptions);
+
+            // Create the User state.  
+            var userState = new UserState(storage);
+            services.AddSingleton<UserState>(userState);
+
+            // Create the Conversation state.  
+            var conversationState = new ConversationState(storage);
+            services.AddSingleton<ConversationState>(conversationState);
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, EchoBot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
